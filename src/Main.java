fd8a539e0151041315dceb06f253e40a1d6daf1b
ysm111 <elegant-belltree@gmail.com>
2014-11-08 14:04:15 +0900

    コメントを修正

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 /**
  * 8進数と16進数で共にゾロ目となるような数を小さい方から3つ表示するプログラム
- * (2進数表現で'1'が12の倍数個並んだ数は8進数と16進数で共にゾロ目になることを利用して求める)
+ * (2進数で'1'が12の倍数個並んだ数は8進数と16進数で共にゾロ目になることを利用して求める)
  */
 public class Main {
     public static void main(String[] args){
         /**
-         * 8進数と16進数で共にゾロ目となるような数の2進数表現
+         * 8進数と16進数で共にゾロ目となるような数の2進数
          */
         StringBuilder binary=new StringBuilder();
         /**
-         * 2進数表現で'1'が12個並んだ数
+         * 2進数で'1'が12個並んだ数
          */
         StringBuilder base=new StringBuilder();
 
         try{
-            //2進数表現で'1'が12個並んだ数の生成
+            //2進数で'1'が12個並んだ数の生成
             for(int i=0;i<12;i++){
                 base.append(1);
             }
 
-            //2進数表現で'1'が12の倍数個並んだ数の生成及び表示
+            //2進数で'1'が12の倍数個並んだ数の生成及び表示
             for(int i=0;i<3;i++){
                 binary.append(base.toString());
                 System.out.println(Long.parseLong(binary.toString(), 2));
